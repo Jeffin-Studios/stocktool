9bdd88effea35317c45a4257c936420c84154e9f
Richard Bao <richardbao419@gmail.com>
2018-09-16 01:05:34 -0700

    starting out

---
+++ test.py
@@ -0,0 +1,16 @@
+from stocktool import stocktool
+
+
+if __name__ == "__main__":
+	name = 'AAPL'
+	start="2015-01-01"
+	end="2018-9-14"
+
+	stock = stocktool(name, start, end)
+	stock.plot_stock()
+	stock.plot_stock(stats=['Daily Change'])
+	model, model_data = stock.create_prophet_model(days=90)
+
+	stock.evaluate_prediction()
+
+
