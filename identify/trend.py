cbb37c2f250c1e0b27b80a99f41d7bcd9d2cde14
Richard Bao <richardbao419@gmail.com>
2018-09-20 18:15:10 -0700

    edit

--- identify/trend.py
+++ identify/trend.py
@@ -5,7 +5,7 @@
 from pandas_datareader import data
 import matplotlib.pyplot as plt
 import pandas as pd
-
+import numpy as np
 
 def rollingAverage(name, start, end, strict = False, graph = False):
 	stock = data.DataReader(name, 'yahoo', start, end)
@@ -39,7 +39,7 @@ def rollingAverage(name, start, end, strict = False, graph = False):
 	return (close_price, short_rolling_stock, medium_rolling_stock)
 
 ######## Need to make this real time, to detect when climb starts and when dip starts
-def fluctuation(name, start, end, graph = False)
+def fluctuation(name, start, end, graph = False):
 	(close_price, short_rolling_stock, medium_rolling_stock) = rollingAverage(name, start, end)
 	change = short_rolling_stock - medium_rolling_stock
 	# Starts climbing when short term average passes long term average
@@ -59,13 +59,42 @@ def fluctuation(name, start, end, graph = False)
 
 	return (buy_dates, sell_dates)
 
+def get_rsi(prices, n=14):
+    deltas = np.diff(prices)
+    seed = deltas[:n+1]
+    up = seed[seed>=0].sum()/n
+    down = -seed[seed<0].sum()/n
+    rs = up/down
+    rsi = np.zeros_like(prices)
+    rsi[:n] = 100. - 100./(1.+rs)
+
+    for i in range(n, len(prices)):
+        delta = deltas[i-1] # cause the diff is 1 shorter
+
+        if delta>0:
+            upval = delta
+            downval = 0.
+        else:
+            upval = 0.
+            downval = -delta
+
+        up = (up*(n-1) + upval)/n
+        down = (down*(n-1) + downval)/n
+
+        rs = up/down
+        rsi[i] = 100. - 100./(1.+rs)
+
+    return rsi
+
 
 if __name__ == "__main__":
 	start="2015-01-01"
 	end="2018-9-20"
 	stocks = ['CRON', 'AMD']
 
-	# (close_price, short_rolling_stock, medium_rolling_stock) = rollingAverage(stocks[1], start, end)
+	(close_price, short_rolling_stock, medium_rolling_stock) = rollingAverage(stocks[1], start, end)
+	rsi = get_rsi(close_price)
+	print(rsi[-1])
 
 
 
