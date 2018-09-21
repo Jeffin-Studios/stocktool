a924f6131bf46949a88c721e13de07d14fa493a9
Richard Bao <richardbao419@gmail.com>
2018-09-20 18:10:54 -0700

    trend

--- identify/trend.py
+++ identify/trend.py
@@ -6,58 +6,66 @@ from pandas_datareader import data
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# We would like all available data from 01/01/2000 until 12/31/2016.
-start="2017-01-01"
-end="2018-9-19"
 
-tickers = ['CRON', 'CELG']
+def rollingAverage(name, start, end, strict = False, graph = False):
+	stock = data.DataReader(name, 'yahoo', start, end)
+	close_price = stock['Close']
 
-# User pandas_reader.data.DataReader to load the desired data. As simple as that.
-stock = data.DataReader(tickers, 'yahoo', start, end)
-# view it
-# stock.to_frame().head(9)
+	if (strict):
+		# Getting all weekdays 
+		all_weekdays = pd.date_range(start=start, end=end, freq='B')
+		close_price = close_price.reindex(all_weekdays)
+		close_price = close_price.fillna(method='ffill')
 
-# Getting just the adjusted closing prices. This will return a Pandas DataFrame
-# The index in this DataFrame is the major index of the stock.
-close = stock['Close']
+	# weekly_roll
+	short_rolling_stock = close_price.rolling(window=5).mean()
+	medium_rolling_stock = close_price.rolling(window=20).mean()
+	long_rolling_stock = close_price.rolling(window=60).mean()
 
-# Getting all weekdays between 01/01/2000 and 12/31/2016
-all_weekdays = pd.date_range(start=start, end=end, freq='B')
+	if (graph):
+		fig, ax = plt.subplots(figsize=(16,9))
 
-# How do we align the existing prices in adj_close with our new set of dates?
-# All we need to do is reindex close using all_weekdays as the new index
-close = close.reindex(all_weekdays)
+		ax.plot(close_price.index, close_price, label=name)
+		ax.plot(short_rolling_stock.index, short_rolling_stock, label='5 days rolling')
+		ax.plot(long_rolling_stock.index, medium_rolling_stock, label='20 days rolling')
+		ax.plot(long_rolling_stock.index, long_rolling_stock, label='60 days rolling')
 
-# Reindexing will insert missing values (NaN) for the dates that were not present
-# in the original set. To cope with this, we can fill the missing by replacing them
-# with the latest available price for each instrument.
-close = close.fillna(method='ffill')
+		ax.set_xlabel('Date')
+		ax.set_ylabel('Adjusted closing price ($)')
+		ax.legend()
+		plt.grid()
+		plt.show()
 
-print(all_weekdays)
+	return (close_price, short_rolling_stock, medium_rolling_stock)
 
-close.head(10)
+######## Need to make this real time, to detect when climb starts and when dip starts
+def fluctuation(name, start, end, graph = False)
+	(close_price, short_rolling_stock, medium_rolling_stock) = rollingAverage(name, start, end)
+	change = short_rolling_stock - medium_rolling_stock
+	# Starts climbing when short term average passes long term average
+	# Starts dipping when short term average goes below long term average
+	buy_dates = []
+	sell_dates = []
 
-close.describe()
+	if (graph):
+		fig, ax = plt.subplots(figsize=(16,9))
+		ax.plot(change.index, change, label="change")
+		ax.set_xlabel('Date')
+		ax.set_ylabel('Difference')
+		ax.legend()
+		plt.axhline(0, color='black')
+		plt.grid()
+		plt.show()
 
-# Get the MSFT timeseries. This now returns a Pandas Series object indexed by date.
-msft = close.loc[:, 'CELG']
+	return (buy_dates, sell_dates)
 
-# Calculate the 20 and 100 days moving averages of the closing prices
-short_rolling_msft = msft.rolling(window=20).mean()
-medium_rolling_msft = msft.rolling(window=60).mean()
-long_rolling_msft = msft.rolling(window=100).mean()
 
-# Plot everything by leveraging the very powerful matplotlib package
-fig, ax = plt.subplots(figsize=(16,9))
+if __name__ == "__main__":
+	start="2015-01-01"
+	end="2018-9-20"
+	stocks = ['CRON', 'AMD']
 
-ax.plot(msft.index, msft, label='CELG')
-ax.plot(short_rolling_msft.index, short_rolling_msft, label='20 days rolling')
-ax.plot(long_rolling_msft.index, medium_rolling_msft, label='60 days rolling')
-ax.plot(long_rolling_msft.index, long_rolling_msft, label='100 days rolling')
+	# (close_price, short_rolling_stock, medium_rolling_stock) = rollingAverage(stocks[1], start, end)
 
-ax.set_xlabel('Date')
-ax.set_ylabel('Adjusted closing price ($)')
-ax.legend()
 
-plt.show()
 
